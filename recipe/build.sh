012ebc53e74453b4d890ca4315cff40ef160187d
Michel Zou <xantares09@hotmail.com>
2018-09-29 11:12:59 +0200

    no openssl

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-./configure --prefix=${PREFIX} --extra-cflags="${CFLAGS}" --extra-ldflags="${LDFLAGS}"
+./configure --prefix=${PREFIX} --extra-cflags="${CFLAGS}" --extra-ldflags="${LDFLAGS}" --disable-openssl
 make -j${CPU_COUNT}
 make install
 make -C librhash install-headers
